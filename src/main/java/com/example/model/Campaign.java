4acc3ae9953c3f52e0e1fc05b2a35c07088abb07
MitsruYakushinji <156178833+MitsruYakushinji@users.noreply.github.com>
2024-02-02 11:46:45 +0900

    No.20 キャンペーン一覧でファイルのアップロード処理を一括更新に変更したい
    importCSV()
    ファイルの読み込み処理
    batchInsertメソッドを修正後に利用して、1行更新から一括更新に修正

--- src/main/java/com/example/model/Campaign.java
+++ src/main/java/com/example/model/Campaign.java
@@ -16,7 +16,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
